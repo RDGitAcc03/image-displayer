2179b8b38804c07fdeb1ba603ac05407c3d5181c
RD <randahn03@gmail.com>
2023-07-03 13:24:12 +0300

    part 3 is done. I wanted to prevent any clicking around the modal when it's opened - not made yet.

--- myscript.js
+++ myscript.js
@@ -2,7 +2,7 @@ const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('iconContainer');
 const imageList = document.getElementById('imageList');
 
-let category, timeout;
+let category, timeout, modal, modalTopPart, modalBottomPart, isModalClosed = true;
 
 function handleDebounceInputCapture() {
     timeout = setTimeout(() => {
@@ -38,22 +38,66 @@ function createImageList(data) {
     imageList.innerHTML = "";
     data.forEach(img => {
         const image = document.createElement('img');
-        const imageContainer = document.createElement('div');
         if (image) {
             image.src = img.webformatURL;
-            imageContainer.classList.add('imageContainer');
-            imageContainer.appendChild(image);
+            const imageContainer = document.createElement('div');
+            if (imageContainer) {
+                imageContainer.classList.add('imageContainer');
+                imageContainer.appendChild(image);
+                imageList.appendChild(imageContainer);
+            }
         }
-        imageList.appendChild(imageContainer);
-        handleOpenModalOnImageClick(imageContainer);
+        if (isModalClosed) {
+            handleOpenModalOnImageClick(image, img);
+        }
     })
 }
 
-function handleOpenModalOnImageClick(imgContainer) {
-    imgContainer.addEventListener('click', () => {
-        const modal = document.createElement('div');
-        modal.classList.add('modal');
+function handleOpenModalOnImageClick(img, imgInfo) {
+    img.addEventListener('click', () => {
+        isModalClosed = false;
+        const imgSrc = img.src;
+        const newImage = document.createElement('img');
+        newImage.src = imgSrc;
+        if (modal) {
+            modal.remove();
+        }
+        if (imageList) {
+            modal = document.createElement('div');
+            isModalClosed = false;
+            modal.style.display = 'block'
+            modal.classList.add('modal');
+            imageList.appendChild(modal);
+            modalTopPart = document.createElement('div');
+            modalBottomPart = document.createElement('div');
+            modal.appendChild(modalTopPart);
+
+            modal.appendChild(modalBottomPart); 
+            if (modalTopPart) {
+                modalTopPart.appendChild(newImage);
+                modalTopPart.innerHTML += '<i class="close fa-regular fa-rectangle-xmark"></i>';
+                const closeModalIcon = document.getElementsByClassName("close")[0];
+                closeModalIcon.addEventListener('click', handleCloseModalOnIconClick);
+            }
+            if (modalBottomPart && imgInfo) {
+                const htmlInfo =
+                    `<p><strong>views</strong>: ${imgInfo.views}</p>
+                     <p><strong>downloads</strong>: ${imgInfo.downloads}</p>
+                     <p><strong>collections</strong>: ${imgInfo.collections}</p>
+                     <p><strong>likes</strong>: ${imgInfo.likes}</p>
+                     <p><strong>comments</strong>: ${imgInfo.comments}</p>
+                    `;
+                modalBottomPart.innerHTML = htmlInfo;
+            }
+        }
     });
+}
+
+function handleCloseModalOnIconClick() {
+    if (!isModalClosed) {
+        modal.style.display = "none";
+        isModalClosed = true;
+    }
 }
 
 searchInput.addEventListener('input', handleDebounceInputCapture);
